b269aac395d5789a1a9fa535f120519e817189b5
utsav.basyal <45966409+utsabbasyal12@users.noreply.github.com>
2023-12-06 17:26:11 +0545

    added identityAPI

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityDotNet8WebAPI
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
